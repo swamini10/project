01bb75c61a01ee2bfea3c6f41459cf8e26a4dd39
swamini <swaminibhandare10@gmail.com>
2026-07-07 20:20:17 +0530

    some issue

--- backend/src/main/java/Study/Assistant/demo/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package Study.Assistant.demo.repository;
-
-import Study.Assistant.demo.entity.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-
-}
